9e0f32789ecb02e1333b59b5ecbd88cfe7d93d7c
Michael Focshaner <mfocshaner@lightricks.com>
2018-08-08 15:26:06 +0300

    test commit - card.m

---
+++ Matchismo/Model/Card.m
@@ -0,0 +1,25 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by Michael Jacob Focshaner on 05/08/2018.
+//  Copyright © 2018 Michael Focshaner. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+- (int)match:(NSArray *)otherCards
+{
+    int score = 0;
+ 
+    for (Card *card in otherCards){
+        if ([card.contents isEqualToString:self.contents]){
+            score = 1;
+        }
+    }
+    return score;
+}
+
+@end
